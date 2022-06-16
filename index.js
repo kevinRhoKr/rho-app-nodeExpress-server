064b249f14f0f19f83f47d2a8e8c7dd0f5fa42c3
kevinRhoKr <kevinrho51@gmail.com>
2022-06-16 09:17:14 -0400

    pretty format and UPDATE feature added

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 const pool = require("./db");
-const cors = require('cors');
+const cors = require("cors");
 const md5 = require("md5");
 
 app.use(express.json()); // => req.body
@@ -11,24 +11,25 @@ app.use(cors());
 
 // create a signup
 app.post("/user/signup", async (req, res) => {
-  
   try {
     const { email, fname, lname, password } = req.body;
 
     //check if there's already a user
-    const user = await pool.query("SELECT f_name FROM appUsers WHERE email = $1", [email]);
+    const user = await pool.query(
+      "SELECT f_name FROM appUsers WHERE email = $1",
+      [email]
+    );
     console.log(user.rows);
 
-    if (user.rows.length === 0){
+    if (user.rows.length === 0) {
       const newUser = await pool.query(
         "INSERT INTO appUsers (email, f_name, l_name, user_password) VALUES ($1, $2, $3, $4) RETURNING *",
         [email, fname, lname, md5(password)]
       );
       res.json(newUser.rows[0]);
     } else {
-      res.json({"status": "fail"});
+      res.json({ status: "fail" });
     }
-
   } catch (err) {
     console.error(err.message);
   }
@@ -37,7 +38,6 @@ app.post("/user/signup", async (req, res) => {
 // log in
 
 app.post("/user/login", async (req, res) => {
-  
   try {
     const { email, password } = req.body;
     console.log(email, password);
@@ -51,27 +51,27 @@ app.post("/user/login", async (req, res) => {
 
     if (user.rows.length == 0) {
       console.log(false);
-      res.json({"status": "false"});
+      res.json({ status: "false" });
     } else {
       console.log(true);
-      res.json({"status": "true"});
+      res.json({ status: "true" });
     }
     //res.json(user.rows[0]);
-
   } catch (err) {
     console.error(err.message);
   }
 });
 
-// get specific user. 
+// get specific user.
 app.post("/user", async (req, res) => {
   try {
-
     const email = req.body.email;
     console.log(email);
 
-    const user = await pool.query("SELECT email, f_name, l_name FROM appUsers WHERE email = $1",
-      [email]);
+    const user = await pool.query(
+      "SELECT email, f_name, l_name FROM appUsers WHERE email = $1",
+      [email]
+    );
     console.log(user);
     res.json(user.rows[0]);
   } catch (err) {
@@ -90,61 +90,72 @@ app.get("/posts", async (req, res) => {
     res.json(query.rows);
   } catch (err) {
     console.log(err.message);
-  } 
-
-
+  }
 });
 
-app.post("/posts/new", async(req, res) => {
+app.post("/posts/new", async (req, res) => {
   const text = req.body.text;
   const email = req.body.email;
-  const currentDate = new Date().toISOString().slice(0,19).replace('T', ' ');
+  const currentDate = new Date().toISOString().slice(0, 19).replace("T", " ");
 
   try {
-
     const find_query = await pool.query(
-      "SELECT user_id FROM appusers WHERE email = $1", 
-      [email]);
-    
+      "SELECT user_id FROM appusers WHERE email = $1",
+      [email]
+    );
+
     const user_id = find_query.rows[0].user_id;
 
     const query = await pool.query(
       "INSERT INTO apposts (user_id, date, body) VALUES ($1, $2, $3) RETURNING *",
       [user_id, currentDate, text]
-    )
+    );
     res.json(query.rows[0]);
     console.log(query);
   } catch (err) {
     console.log(err.message);
   }
-})
+});
 
-app.delete("/posts/delete", async(req, res) => {
+app.delete("/posts/delete", async (req, res) => {
   const post_id = req.body.post_id;
 
-try {
-  const delete_query = await pool.query(
-    "DELETE FROM apposts WHERE post_id = $1",
-    [post_id]
-  )
-  res.json(delete_query);
+  try {
+    const delete_query = await pool.query(
+      "DELETE FROM apposts WHERE post_id = $1",
+      [post_id]
+    );
+    res.json(delete_query);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-} catch (error) {
-  console.log(error);
-}
+app.put("/posts/update", async (req, res) => {
+  const post_id = req.body.post_id;
+  const body = req.body.body;
+  const date = new Date().toISOString().slice(0, 19).replace("T", " ");
 
-})
+  try {
+    const update_query = await pool.query(
+      "UPDATE apposts SET body = $1, date = $2 WHERE post_id = $3",
+      [body, date, post_id]
+    );
+    res.json(update_query);
+  } catch (error) {
+    console.log(error);
+  }
+});
 
-app.post("/contact/new", async(req, res) => {
+app.post("/contact/new", async (req, res) => {
   const text = req.body.text;
   const email = req.body.email;
 
   try {
-
     const find_user_query = await pool.query(
       "SELECT user_id FROM appusers WHERE email = $1",
       [email]
-      );
+    );
     const user_id = find_user_query.rows[0].user_id;
 
     const query = await pool.query(
@@ -152,16 +163,12 @@ app.post("/contact/new", async(req, res) => {
       [text, user_id]
     );
     res.json(query.rows[0]);
-
   } catch (error) {
     console.log(error.message);
   }
+});
 
-
-})
-
-app.get("/contact/requests", async(req, res) => {
-
+app.get("/contact/requests", async (req, res) => {
   try {
     const requests_query = await pool.query(
       "SELECT f_name, l_name, email, body FROM requests NATURAL JOIN appusers"
@@ -171,7 +178,7 @@ app.get("/contact/requests", async(req, res) => {
   } catch (error) {
     console.log(error);
   }
-})
+});
 
 app.listen(3000, () => {
   console.log("server is listening on port 3000");
